247a048c9a64ec2da41957d02a8dbe1042d140e4
taashev <taashev.a.r92@yandex.ru>
2023-02-27 12:45:48 +0300

    feat: routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('./utils/cors');
+const router = require('./routes/router');
 
 const { PORT, NODE_ENV } = require('./config');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors);
+app.use('/api', router);
 
 app.listen(PORT, (err) => {
   if (NODE_ENV !== 'production')
---
+++ backend/routes/router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/history', (req, res) => {
+  res.send();
+});
+router.post('/text', (req, res) => {
+  res.send();
+});
+
+module.exports = router;
